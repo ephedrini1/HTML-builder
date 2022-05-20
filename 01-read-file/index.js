eb0f9591a7fabcb3edcd9816fd3929401a3f11cd
ephedrini1 <ephedrini@yandex.ru>
2022-05-20 10:48:51 +0400

    feat: 01-read-file complete

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+const {stdout} = process;
+
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'));
+stream.on('data', data => {
+  stdout.write(data.toString());
+});
