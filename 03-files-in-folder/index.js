a055ee62f4258ff89a1753031cdcb140c670db8e
ephedrini1 <ephedrini@yandex.ru>
2022-05-20 16:15:59 +0400

    feat: 04-copy-directory complete

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,13 +3,18 @@ const path = require('path');
 const { stdout } = process;
 
 
-(async () => {
+(async() =>  {
   const folderPath = path.join(__dirname, 'secret-folder');
-  const files = await readdir(folderPath, { withFileTypes: true });
-  for (const file of files) {
-    if (file.isFile()) {
-      const stats = await stat(path.join(folderPath, file.name));
-      stdout.write(`${path.parse(file.name).name} - ${path.extname(file.name).slice(1)} - ${stats.size}bytes\n`);
+
+  try {
+    const files = await readdir(folderPath, { withFileTypes: true });
+    for (const file of files) {
+      if (file.isFile()) {
+        const stats = await stat(path.join(folderPath, file.name));
+        stdout.write(`${path.parse(file.name).name} - ${path.extname(file.name).slice(1)} - ${stats.size}b\n`);
+      }
     }
+  } catch (error) {
+    stdout.write(`Error: ${error.message}\n`);
   }
 })();
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const { readdir, copyFile, mkdir } = require('fs').promises;
+const path = require('path');
+const { stdout } = process;
 
+const pathFrom = path.join(__dirname, 'files');
+const pathTo = path.join(__dirname, 'files-copy');
+
+async function copyDir(from, to) {
+  try {
+    const files = await readdir(from, { withFileTypes: true });
+    for (const file of files) {
+      if (file.isFile()) {
+        await copyFile(path.join(from, file.name), path.join(to, file.name));
+      } else if (file.isDirectory()) {
+        await mkdir(path.join(to, file.name));
+        await copyDir(path.join(from, file.name), path.join(to, file.name));
+      }
+    }
+  } catch (error) {
+    stdout.write(error.message);
+  }
+}
+
+(async function () {
+  await mkdir(pathTo, { recursive: true });
+  await copyDir(pathFrom, pathTo);
+})();
